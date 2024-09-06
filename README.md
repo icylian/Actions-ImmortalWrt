0f32d792bed882837226fd226db364a21b3aeb88
icylian <red4357@gmail.com>
2024-09-06 21:41:35 +0900

    Add luci-app-mosdns.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ImmortalWrt Actions For NanoPi R6S
 ## Notes
 1. ROOTFS_PARTSIZE=180 => 1024
-2. Only include Luci WebUI, Docker and Mihomo.
+2. Only include Luci WebUI, Docker, Mosdns and Mihomo.
 ## [Original README](https://github.com/bling-yshs/Actions-ImmortalWrt)
--- diy.sh
+++ diy.sh
@@ -3,3 +3,12 @@
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 # git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo.git package/mihomo
+
+# luci-app-mosdns
+# drop mosdns and v2ray-geodata packages that come with the source
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# make menuconfig # choose LUCI -> Applications -> luci-app-mosdns
+# make package/mosdns/luci-app-mosdns/compile V=s
